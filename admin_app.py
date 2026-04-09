6ba8ee0f3eb8713c42bdb80744a38d408f028217
jcoetee-noticeboard <jcoeteenoticeboard@gmail.com>
2026-04-09 19:51:04 +0530

    Changes in admin_app

--- admin_app.py
+++ admin_app.py
@@ -12,7 +12,7 @@ HEADERS = {"API-KEY": API_KEY}
 # ---------------- HOME (UPLOAD PAGE) ----------------
 @app.route('/')
 def index():
-    return render_template('templates/admin.html')
+    return render_template('admin.html')
 
 
 # ---------------- CREATE (UPLOAD) ----------------
