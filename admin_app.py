2773572d79cdaefb34820cf8ace5e18ada3164ed
jcoetee-noticeboard <jcoeteenoticeboard@gmail.com>
2026-04-09 20:26:55 +0530

    Adding ngrok URL in admin_app

--- admin_app.py
+++ admin_app.py
@@ -4,7 +4,7 @@ import requests
 app = Flask(__name__)
 app.secret_key = "supersecretkey"
 
-BASE_URL = "http://192.168.137.251:5000"
+BASE_URL = "http://127.0.0.1:4040"
 API_KEY = "mysecret"
 
 HEADERS = {"API-KEY": API_KEY}
