0aa7aae4310f856b2ef263d60801d7b970cb6307
jcoetee-noticeboard <jcoeteenoticeboard@gmail.com>
2026-04-09 23:13:08 +0530

    Cloudflare URL modifications in admin_app

--- admin_app.py
+++ admin_app.py
@@ -4,7 +4,7 @@ import requests
 app = Flask(__name__)
 app.secret_key = "supersecretkey"
 
-BASE_URL = "https://micrographic-meg-unaccustomedly.ngrok-free.dev"
+BASE_URL = "https://democrat-allowed-provided-mods.trycloudflare.com"
 API_KEY = "mysecret"
 
 HEADERS = {"API-KEY": API_KEY}
